cf1e356050f02587065130fe43d31ac6e1be61a1
Aimar Aareleid <aimar@snapit.ee>
2018-01-16 12:52:59 +0100

    first commit

---
+++ src/shared/App.js
@@ -0,0 +1,16 @@
+import react, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React in the server</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
